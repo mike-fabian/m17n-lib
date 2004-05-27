c3dd6cb0a0e6fffc44d66720876d1381e15f244c
Kenichi Handa <handa@gnu.org>
2004-05-27 09:20:44 +0000

    Clean old files at first.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@
 #   Registration Number H15PRO112
 # See the end for copying conditions.
 
+echo "Cleaning up old files..."
+rm -rf INSTALL aclocal.m4 autom4te.cache config.guess. config.sub install-sh ltmain.sh missing mkinstalldirs libtool
 echo "Running libtoolize..."
 libtoolize --automake
 echo "Running aclocal..."
