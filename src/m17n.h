47ac5299df5d9ccc2c885082a833f5d25a349f74
Kenichi Handa <handa@gnu.org>
2014-03-11 17:33:49 +0000

    m17n.h (minput_parse_im_names): Extern it.

--- src/m17n.h
+++ src/m17n.h
@@ -1389,6 +1389,8 @@ extern MPlist *minput_get_variables (MSymbol language, MSymbol name);
 extern int minput_set_variable (MSymbol language, MSymbol name,
 				MSymbol variable, void *value);
 
+extern MPlist *minput_parse_im_names (MText *mt);
+
 extern MPlist *minput_list (MSymbol lang);
 
 extern MInputMethod *mdebug_dump_im (MInputMethod *im, int indent);
